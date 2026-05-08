06b0976a350132eea5348747a52ae88bbc2c6e1a
nathanalba <nathan.albanese@gmail.com>
2026-05-08 08:37:22 -0400

    initial

--- main.go
+++ main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"fmt"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+func main() {
+	p := tea.NewProgram(initialModel(), tea.WithAltScreen())
+	if _, err := p.Run(); err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+}
